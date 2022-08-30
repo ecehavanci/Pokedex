06557b402fb4cd487e582e7459f8061f1195e149
ecehavanci <ecehavanci@gmail.com>
2022-08-30 15:12:25 +0300

    redux tried on battle page

--- src/components/Battle/Battle.js
+++ src/components/Battle/Battle.js
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount } from '../../reducers/counterSlice';
 
-function Battle(){
-    return(<p>fsdfds</p>);
+function Battle() {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <p> value: {count}</p>
+        </div>
+    );
 }
 export default Battle;
+//
